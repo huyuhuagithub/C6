6201eb00e5143825704a4dafc89a30314981de12
胡裕华 <489567501@qq.com>
2022-12-13 22:25:56 +0800

    学习第2章
    使用 visual  studio code 写第一个代码

---
+++ ch02/02_01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+    int num=10;
+    printf("my first code ");
+    printf("for c\n");
+    printf("num is \t%d Because it is Being",num);
+    getchar();/*暂停程序显示*/
+    return 0;
+    /*注释使用示例1*/
+
+    
+    /* 
+    注释使用示例2
+    */
+}
