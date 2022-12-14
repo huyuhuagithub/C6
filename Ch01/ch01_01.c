d7c1aca4594d65835cdcf2a6fa5a93f5e73e9119
胡裕华 <489567501@qq.com>
2022-12-14 21:54:57 +0800

    测试在多台电脑修改代码
    测试在多台电脑修改代码

--- Ch01/ch01_01.c
+++ Ch01/ch01_01.c
@@ -1,6 +1,13 @@
-#include "stdio.h"
-int main()
+#include <stdio.h>
+int main(void)
 {
-    printf("hello world!");
+
+    //测试在多台电脑修改代码
+    int num=0;
+    printf("Enter number:\t");
+    scanf("%d",&num);
+    printf("\tinput num it is %d\n",num);
+    /*printf("\thello world!");*/
+    getchar();
     return 0;
 } 
